cbf7443cfb562ff666d9fcfd1fe70fe346620ac8
Admin <elena.pozitiffa@yandex.ru>
2020-10-03 21:19:38 +0300

    My Django Girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ncuia(is)q3xpn#t2ar__w@2nhh&l1f((bdktx^klr^u=-$%fa'
+SECRET_KEY = 'bc()1ul0g=p5mkexzm_m#&9$7ir$5e%yg@%zr6=bl-vxvbzlt*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanewhere.com']
 
 
 # Application definition
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
-
+	'blog',
 ]
 
 MIDDLEWARE = [
@@ -120,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
